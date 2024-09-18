6ca276ce9ad3c3cf1f5ad61f79a057e6c4645efa
asierzapata <asier.zapata@gmail.com>
2024-09-18 07:50:30 +0200

    Update sketchybar init to only run on install

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -3,14 +3,14 @@
 export WHITE=0xffffffff
 
 # -- Teal Scheme --
-export BAR_COLOR=0x00f17013
-export ITEM_BG_COLOR=0xff003547
-export ACCENT_COLOR=0xff2cf9ed
+# export BAR_COLOR=0x00f17013
+# export ITEM_BG_COLOR=0xff003547
+# export ACCENT_COLOR=0xff2cf9ed
 
 # -- Gray Scheme --
-# export BAR_COLOR=0x00f17013
-# export ITEM_BG_COLOR=0xff353c3f
-# export ACCENT_COLOR=0xffffffff
+export BAR_COLOR=0x80808080
+export ITEM_BG_COLOR=0xff353c3f
+export ACCENT_COLOR=0xffffffff
 
 # -- Purple Scheme --
 # export BAR_COLOR=0x00f17013
@@ -22,7 +22,7 @@ export ACCENT_COLOR=0xff2cf9ed
 # export ITEM_BG_COLOR=0xff591221
 # export ACCENT_COLOR=0xffff2453
 
-# -- Blue Scheme --- 
+# -- Blue Scheme ---
 # export BAR_COLOR=0xff23090e
 # export ITEM_BG_COLOR=0xff093aa8
 # export ACCENT_COLOR=0xff15bdf9
--- sketchybar/path.zsh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p "$HOME/.config/sketchybar"
-cp -rf $HOME/.dotfiles/sketchybar/* $HOME/.config/sketchybar
