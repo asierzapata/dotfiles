dfe4de9568d201567a73b15b815daaaf07c85d92
asierzapata <asier.zapata@gmail.com>
2025-02-18 20:58:13 +0100

    feature: Improve copilot chat plugin

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,11 +1,14 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    -- leaving it empty uses the default settings
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+	opts = {
+		-- leaving it empty uses the default settings
+		spec = {
+			{ "<leader>a", group = "ai" },
+		},
+	},
 }
