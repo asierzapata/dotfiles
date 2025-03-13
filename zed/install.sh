b58a762732800bdc40b048409c184a408f52da2b
asierzapata <asier.zapata@gmail.com>
2025-03-13 12:28:38 +0100

    chore: change some zed settings and bring changes from fork

--- zed/install.sh
+++ zed/install.sh
@@ -1,4 +1,8 @@
 mkdir -p "$HOME/.config/zed"
+
 rm -rf $HOME/.config/zed/*
 
+mkdir -p "$HOME/.config/zed/snippets"
+
 ln -s "$HOME/.dotfiles/zed/settings.json" "$HOME/.config/zed/settings.json"
+ln -s "$HOME/.dotfiles/zed/snippets/javascript.json" "$HOME/.config/zed/snippets/javascript.json"
