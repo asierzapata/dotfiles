67034b0860e31045ec2d92f543d982fc7c9cf58b
asierzapata <asier.zapata@gmail.com>
2025-06-05 07:36:03 +0200

    feat: configure mac exclusive packages to only be installed when Darwin is detected

--- skhd/install.sh
+++ skhd/install.sh
@@ -1,5 +1,8 @@
-mkdir -p "$HOME/.config/skhd"
-rm -rf $HOME/.config/skhd/*
-cp -rf $HOME/.dotfiles/skhd/* $HOME/.config/skhd
+if [ "$(uname -s)" == "Darwin" ]
+then
+	mkdir -p "$HOME/.config/skhd"
+	rm -rf $HOME/.config/skhd/*
+	cp -rf $HOME/.dotfiles/skhd/* $HOME/.config/skhd
 
-skhd --start-service
+	skhd --start-service
+fi
--- yabai/install.sh
+++ yabai/install.sh
@@ -1,5 +1,8 @@
+if [ "$(uname -s)" == "Darwin" ]
+then
 mkdir -p "$HOME/.config/yabai"
 rm -rf $HOME/.config/yabai/*
 cp -rf $HOME/.dotfiles/yabai/* $HOME/.config/yabai
 
 yabai --start-service
+fi
