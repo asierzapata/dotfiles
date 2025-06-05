9859999702efca1728cedff30bd65c2e791f7c96
asierzapata <asier.zapata@gmail.com>
2025-06-05 07:36:03 +0200

    feat: configure mac exclusive packages to only be installed when Darwin is detected

--- sketchybar/install.sh
+++ sketchybar/install.sh
@@ -1,5 +1,9 @@
-mkdir -p "$HOME/.config/sketchybar"
-cp -rf $HOME/.dotfiles/sketchybar/* $HOME/.config/sketchybar
+# If we're on a Mac, let's install and setup homebrew.
+if [ "$(uname -s)" == "Darwin" ]
+then
+	mkdir -p "$HOME/.config/sketchybar"
+	cp -rf $HOME/.dotfiles/sketchybar/* $HOME/.config/sketchybar
 
-brew services restart sketchybar
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.16/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+	brew services restart sketchybar
+	curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.16/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+fi
