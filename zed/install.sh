aad627f75c17fd94713981b01a5673c0a7a98f70
asierzapata <asier.zapata@gmail.com>
2025-12-04 09:21:38 +0100

    update zed settings to add tasks

--- zed/install.sh
+++ zed/install.sh
@@ -5,4 +5,5 @@ rm -rf $HOME/.config/zed/*
 mkdir -p "$HOME/.config/zed/snippets"
 
 ln -s "$HOME/.dotfiles/zed/settings.json" "$HOME/.config/zed/settings.json"
+ln -s "$HOME/.dotfiles/zed/tasks.json" "$HOME/.config/zed/tasks.json"
 ln -s "$HOME/.dotfiles/zed/snippets/javascript.json" "$HOME/.config/zed/snippets/javascript.json"
